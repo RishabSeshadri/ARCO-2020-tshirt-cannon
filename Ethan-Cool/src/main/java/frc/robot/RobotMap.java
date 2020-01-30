a1db3d2f9d96919d72c0a313d46c1e2384988c6e
RaidersOfTheARC <48812888+RaidersOfTheARC@users.noreply.github.com>
2020-01-29 19:35:27 -0500

    Update RobotMap.java

--- Ethan-Cool/src/main/java/frc/robot/RobotMap.java
+++ Ethan-Cool/src/main/java/frc/robot/RobotMap.java
@@ -1,6 +1,8 @@
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import edu.wpi.first.wpilibJ.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Compressor;
 
 public class RobotMap {
 
@@ -9,6 +11,9 @@ public class RobotMap {
     public static VictorSPX CANNON_SPINNER; // ? JAXON_TALL
     public static LiftyThingIdkBruhItsJustAName SPINNY_BOI; // ? LIFTY_BOI
     public static XboxController RISHAB;
+    public static DoubleSolenoid D_SOLENOID;
+    public static Compressor AIR_BOI
+
 
     public static void init() {
         FRONT_LEFT = new TalonSRX(10);
@@ -19,6 +24,9 @@ public class RobotMap {
         CANNON_SPINNER = new VictorSPX(17);
         // ? JAXON_TALL = new VictorSPX(23);
 
+        D_SOLENOID = new DoubleSolenoid(1, 1);
+        AIR_BOI = new Compressor(0);
+
         RISHAB = new XboxController(0);
     
         KUUNAV_DRIVE = new MecanumDrive(FRONT_LEFT, FRONT_RIGHT, BACK_LEFT, BACK_RIGHT);
@@ -26,6 +34,6 @@ public class RobotMap {
         SPINNY_BOI = new LiftyThingIdkBruhItsJustAName(CANNON_SPINNER);
         // ? LIFTY_BOI = new LiftyThingIdkBruhItsJustAName(JAXON_TALL);
 
-        // ! TESTING COMMENTS, TESTING GIT, IGNORE COMMIT
+        
     }
 }
